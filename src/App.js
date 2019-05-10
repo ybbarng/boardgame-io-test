72449c01855f7a7cacf681ca30586c9c19f570b2
ybbarng <ybbarng@gmail.com>
2019-05-10 15:10:19 +0900

    Focus on onTurnBegin() of phase2 is not calling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 import { Client } from 'boardgame.io/react';
-import { Game } from 'boardgame.io/core';
+import { Game, TurnOrder } from 'boardgame.io/core';
 
 const Test = Game({
-  setup: () => ({ key0: 'value0' }),
+  setup: () => ({}),
   flow: {
     startingPhase: 'phase1',
     phases: {
       phase1: {
-        // Always onTurnBegin() is called, even endPhase() is called in onPhaseBegin()
-        // To call endPhaseIf(), endTurn() must be called
-        endPhaseIf: G => {
-          console.log('phase1 endPhaseIf');
-          return Boolean(G.key1);
-        },
         onPhaseBegin: (G, ctx) => {
           console.log('phase1 onPhaseBegin');
-          G.key0 = 'value1';
-          G.key1 = 'value1';
         },
         onPhaseEnd: (G, ctx) => {
           console.log('phase1 onPhaseEnd');
         },
         onTurnBegin: (G, ctx) => {
-          console.log('phase1 onTurnBegin');
-          console.log(G.key1);
+          console.log('phase1 onTurnBegin ' + ctx.currentPlayer);
           ctx.events.endTurn();
         },
+        onTurnEnd: (G, ctx) => {
+          console.log('phase1 onTurnEnd ' + ctx.currentPlayer);
+        },
+        turnOrder: TurnOrder.ONCE,
         next: 'phase2'
       },
       phase2: {
-        // onTurnBegin() is not called always
-        // endPhaseIf() is called always
-        endPhaseIf: G => {
-          console.log('phase2 endPhaseIf');
-          console.log(G.key2);
-          return Boolean(G.key2) && false;
-        },
         onPhaseBegin: (G, ctx) => {
           console.log('phase2 onPhaseBegin');
-          G.key0 = 'value2';
-          G.key1 = 'value2';
-          G.key2 = 'value2';
         },
         onPhaseEnd: (G, ctx) => {
           console.log('phase2 onPhaseEnd');
@@ -48,27 +33,9 @@ const Test = Game({
         onTurnBegin: (G, ctx) => {
           // Not be called
           console.log('phase2 onTurnBegin');
-          console.log(G.key2);
         },
-        next: 'phase3'
+        turnOrder: TurnOrder.ONCE,
       },
-      phase3: {
-        onPhaseBegin: (G, ctx) => {
-          console.log('phase3 onPhaseBegin');
-          G.key0 = 'value3';
-          G.key1 = 'value3';
-          G.key2 = 'value3';
-          G.key3 = 'value3';
-        },
-        onPhaseEnd: (G, ctx) => {
-          console.log('phase3 onPhaseEnd');
-        },
-        onTurnBegin: (G, ctx) => {
-          console.log('phase3 onTurnBegin');
-          console.log(G.key2);
-          console.log(G.key3);
-        }
-      }
     }
   }
 });
